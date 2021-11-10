366c028e8574cc1dae315c8e9dbb0138e2faa118
PheonixAhn <ajk78978@naver.com>
2021-11-11 00:22:12 +0900

    detail template 수정

--- my_favorite_item/public/js/app.js
+++ my_favorite_item/public/js/app.js
@@ -2407,6 +2407,9 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   mounted: function mounted() {}
 });
@@ -39159,77 +39162,58 @@ var staticRenderFns = [
       ]),
       _vm._v(" "),
       _c("div", { staticClass: "media text-muted pt-3" }, [
-        _c("img", {
-          staticClass: "mr-2 rounded",
-          staticStyle: { width: "32px", height: "32px" },
-          attrs: {
-            "data-src":
-              "holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1",
-            alt: "32x32",
-            src: "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2232%22%20height%3D%2232%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2032%2032%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17cfffbb382%20text%20%7B%20fill%3A%23007bff%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A2pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17cfffbb382%22%3E%3Crect%20width%3D%2232%22%20height%3D%2232%22%20fill%3D%22%23007bff%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2211.5390625%22%20y%3D%2216.9%22%3E32x32%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-            "data-holder-rendered": "true",
-          },
-        }),
-        _vm._v(" "),
-        _c(
-          "p",
-          {
-            staticClass:
-              "media-body pb-3 mb-0 small lh-125 border-bottom border-gray",
-          },
-          [
-            _c("strong", { staticClass: "d-block text-gray-dark" }, [
-              _vm._v("@username"),
+        _c("div", { staticClass: "col-md-4" }, [
+          _c("div", { staticClass: "card mb-4 box-shadow" }, [
+            _c("img", {
+              staticClass: "card-img-top",
+              attrs: {
+                src: "https://sc3.locondo.jp/contents/commodity_image/CO/CO6840EU00011_1_m.jpg",
+                alt: "Card image cap",
+              },
+            }),
+            _vm._v(" "),
+            _c("div", { staticClass: "card-body" }, [
+              _c("p", { staticClass: "card-text" }, [
+                _vm._v(
+                  "This is a wider card with supporting text below as a natural lead-in to additional content.\r\n                        This content is a little bit longer."
+                ),
+              ]),
+              _vm._v(" "),
+              _c(
+                "div",
+                {
+                  staticClass:
+                    "d-flex justify-content-between align-items-center",
+                },
+                [
+                  _c("div", { staticClass: "btn-group" }, [
+                    _c(
+                      "button",
+                      {
+                        staticClass: "btn btn-sm btn-outline-secondary",
+                        attrs: { type: "button" },
+                      },
+                      [_vm._v("View")]
+                    ),
+                    _vm._v(" "),
+                    _c(
+                      "button",
+                      {
+                        staticClass: "btn btn-sm btn-outline-secondary",
+                        attrs: { type: "button" },
+                      },
+                      [_vm._v("Edit")]
+                    ),
+                  ]),
+                  _vm._v(" "),
+                  _c("small", { staticClass: "text-muted" }, [
+                    _vm._v("12 mins"),
+                  ]),
+                ]
+              ),
             ]),
-            _vm._v(
-              "\r\n        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.\r\n        "
-            ),
-          ]
-        ),
-      ]),
-      _vm._v(" "),
-      _c("div", { staticClass: "media text-muted pt-3" }, [
-        _c("img", {
-          staticClass: "mr-2 rounded",
-          staticStyle: { width: "32px", height: "32px" },
-          attrs: {
-            "data-src":
-              "holder.js/32x32?theme=thumb&bg=e83e8c&fg=e83e8c&size=1",
-            alt: "32x32",
-            src: "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2232%22%20height%3D%2232%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2032%2032%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17cfffbb385%20text%20%7B%20fill%3A%23e83e8c%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A2pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17cfffbb385%22%3E%3Crect%20width%3D%2232%22%20height%3D%2232%22%20fill%3D%22%23e83e8c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2211.5390625%22%20y%3D%2216.9%22%3E32x32%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-            "data-holder-rendered": "true",
-          },
-        }),
-        _vm._v(" "),
-        _c(
-          "p",
-          {
-            staticClass:
-              "media-body pb-3 mb-0 small lh-125 border-bottom border-gray",
-          },
-          [
-            _c("strong", { staticClass: "d-block text-gray-dark" }, [
-              _vm._v("@username"),
-            ]),
-            _vm._v(
-              "\r\n        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.\r\n        "
-            ),
-          ]
-        ),
-      ]),
-      _vm._v(" "),
-      _c("div", { staticClass: "media text-muted pt-3" }, [
-        _c("img", {
-          staticClass: "mr-2 rounded",
-          staticStyle: { width: "32px", height: "32px" },
-          attrs: {
-            "data-src":
-              "holder.js/32x32?theme=thumb&bg=6f42c1&fg=6f42c1&size=1",
-            alt: "32x32",
-            src: "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2232%22%20height%3D%2232%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2032%2032%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17cfffbb387%20text%20%7B%20fill%3A%236f42c1%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A2pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17cfffbb387%22%3E%3Crect%20width%3D%2232%22%20height%3D%2232%22%20fill%3D%22%236f42c1%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2211.5390625%22%20y%3D%2216.9%22%3E32x32%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-            "data-holder-rendered": "true",
-          },
-        }),
+          ]),
+        ]),
         _vm._v(" "),
         _c(
           "p",
